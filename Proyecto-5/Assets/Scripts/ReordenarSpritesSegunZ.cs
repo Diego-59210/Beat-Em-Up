e9c014701fa1fc15445422edbffe1a4a5b66efc5
Soobi21 <diegomartinez59210@gmail.com>
2022-11-02 15:53:20 -0300

    Script para Reordenar Capas

---
+++ Proyecto-5/Assets/Scripts/ReordenarSpritesSegunZ.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class ReordenarSpritesSegunZ : MonoBehaviour
+{
+
+    SpriteRenderer[] sr;
+    int[] ordenOriginal;
+    private void Start()
+    {
+        sr = GetComponentsInChildren<SpriteRenderer>();
+        ordenOriginal = new int[sr.Length];
+        for (int i = 0; i < sr.Length; i++)
+        {
+            ordenOriginal[i] = sr[i].sortingOrder;
+        }
+    }
+
+
+    private void Update()
+    {
+        for (int i = 0; i < sr.Length; i++)
+        {
+            sr[i].sortingOrder = (int) transform.position.z *  ordenOriginal[i];
+        }
+    }
+}
